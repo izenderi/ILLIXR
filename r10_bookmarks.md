9dfe667ae93fb9137106acaf7f9e9b915d27a788
izenderi <ziliang1121@gmail.com>
2024-01-11 17:55:42 -0800

    auto_run

--- r10_bookmarks.md
+++ r10_bookmarks.md
@@ -5,3 +5,7 @@ plugins/timewarp_gl/plugin.cpp line 846 latency_rtd, latency_mtd ...
 
 ## IMU integration control
 plugins/gtsam_integrator/plugin.cpp line 204 //// Need to integrate over a sliding window of 2 imu_type values.
+
+# Make sure to set the following before running exp on Xavier
+* jtop: set the power mode from the GUI or here
+* jtop: jetson_clocks has to run to fix frequency (6 ctrl in jtop)
--- run.sh
+++ run.sh
@@ -1,1 +1,25 @@
-sudo main.dbg.exe --plugins=offline_imu,offline_cam,gtsam_integrator,pose_prediction,ground_truth_slam,gldemo,timewarp_gl,debugview,offload_data,audio_pipeline --vis=openvins --data=/home/ziliang/data/MH_01/mav0 --demo_data=/home/ziliang/Developer/Git/ILLIXR/demo_data --enable_offload=false --enable_alignment=false --enable_verbose_errors=false --enable_pre_sleep=false
+#!/bin/bash
+
+# Check if the number of repetitions is provided
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <number_of_repetitions>"
+    exit 1
+fi
+
+# Number of times to execute the command
+n=$1
+
+# Loop and execute the command n times, logging each execution to a separate file
+for (( i=1; i<=n; i++ ))
+do
+    logfile="log_MH05_${i}.txt"
+    echo "Starting exp$i..."
+
+    # Special Variable SECONDS
+    SECONDS=0
+    main.dbg.exe --plugins=offline_imu,offline_cam,gtsam_integrator,pose_prediction,ground_truth_slam,gldemo,timewarp_gl,debugview,offload_data,audio_pipeline --vis=openvins --data=/home/ziliang/data/MH_05/mav0 --demo_data=/home/ziliang/Developer/Git/ILLIXR/demo_data --enable_offload=false --enable_alignment=false --enable_verbose_errors=false --enable_pre_sleep=false > "$logfile" 2>&1
+    # main.dbg.exe --plugins=offline_imu,offline_cam,gtsam_integrator,pose_prediction,ground_truth_slam,gldemo,timewarp_gl,debugview,offload_data,audio_pipeline --vis=openvins --data=/experiment/Data/MH05/mav0 --demo_data=/experiment/Developer/ILLIXR/demo_data --enable_offload=false --enable_alignment=false --enable_verbose_errors=false --enable_pre_sleep=false > "$logfile" 2>&1
+
+    duration=$SECONDS
+    echo "exp$i Finished in $duration seconds"
+done
