38b691449b4e23358145ac30742c50af8c72be32
izenderi <ziliang1121@gmail.com>
2024-03-18 12:50:09 -0700

    rebuild update

--- rebuild.sh
+++ rebuild.sh
@@ -5,6 +5,6 @@
 
 rm -rf build/ && mkdir build && cp data.zip ./build && cd build
 cmake .. -DCMAKE_BUILD_TYPE=Debug -DYAML_FILE=profiles/native_gl.yaml
-cmake --build . -j12
+cmake --build . -j$(nproc)
 cmake --build . -t docs 
 cmake --install .
