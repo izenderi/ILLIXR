efaddd2b904a5cfb192bae7012d9d10f217de037
izenderi <ziliang1121@gmail.com>
2025-04-16 12:40:33 -0700

    monado integration

--- init_build.sh
+++ init_build.sh
@@ -5,7 +5,7 @@ rm -rf build/ && mkdir build && cd build
 # when you obtained the data.zip file, you copy it to repo root and use below:
 # rm -rf build/ && mkdir build && cp data.zip ./build && cd build
 
-cmake .. -DCMAKE_BUILD_TYPE=Debug -DYAML_FILE=profiles/native_gl.yaml
+cmake .. -DCMAKE_BUILD_TYPE=Debug -DYAML_FILE=profiles/monado_vk_zed.yaml
 cmake --build . -j$(nproc)
 cmake --build . -t docs
 cmake --install .
